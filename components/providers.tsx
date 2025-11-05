c32e636345395c6c8f7a5f73c7f4fd64fdf6bed5
Feka7 <feka7@hotmail.it>
2025-11-05 17:42:45 +0100

    downgrade next js for problem in Link navigation

--- components/providers.tsx
+++ components/providers.tsx
@@ -8,6 +8,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       height="4px"
       color="#0d9488"
       options={{ showSpinner: false }}
+      delay={200}
     >
       {children}
     </ProgressProvider>
